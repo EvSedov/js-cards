de6fb870816b0d4861e9600d653b7a494d7b05ad
Evgeniy Sedov <kokchix@gmail.com>
2021-06-16 06:24:34 +0700

    create drag and drop element

---
+++ 02 drag-and-drop/app.js
@@ -0,0 +1,38 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+placeholders.forEach((placeholder) => {
+  placeholder.addEventListener('dragenter', dragenter);
+  placeholder.addEventListener('dragover', dragover);
+  placeholder.addEventListener('dragleave', dragleave);
+  placeholder.addEventListener('drop', dragdrop);
+});
+
+function dragstart (e) {
+  e.target.classList.add('hold');
+  setTimeout(() => e.target.classList.add('hide'), 0);
+};
+
+function dragend (e) {
+  e.target.className = 'item';
+};
+
+function dragenter (e) {
+  e.target.classList.add('hovered');
+};
+
+function dragleave (e) {
+  e.target.classList.remove('hovered');
+};
+
+function dragover (e) {
+  e.preventDefault();
+};
+
+function dragdrop (e) {
+  e.target.classList.remove('hovered');
+  e.target.append(item);
+};
